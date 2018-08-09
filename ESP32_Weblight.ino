18c6980cb15e05c3c4381bea1ed353bf197a0141
Kevin Porras Rowe <35790252+Kevin7020@users.noreply.github.com>
2018-08-08 20:55:32 -0600

    Initial Commit

---
+++ ESP32_Weblight.ino
@@ -0,0 +1,24 @@
+int freq = 6000;
+int ledChannel = 0;
+int resolution = 8;
+ 
+void setup() {
+ 
+  ledcSetup(ledChannel, freq, resolution);
+  ledcAttachPin(22, ledChannel);
+ 
+}
+ 
+void loop() {
+ 
+  for (int dutyCycle = 0; dutyCycle <= 255; dutyCycle++) {
+    ledcWrite(ledChannel, dutyCycle);
+    delay(30);
+  }
+ 
+  for (int dutyCycle = 255; dutyCycle >= 0; dutyCycle--) {
+    ledcWrite(ledChannel, dutyCycle);
+    delay(30);
+  }
+ 
+}
